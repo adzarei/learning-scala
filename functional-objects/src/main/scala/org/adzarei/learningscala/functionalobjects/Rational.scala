bd558329032acd1f887a9f5cd5266bb9cf2bb45d
Adrian <adrianzapater@gmail.com>
2020-02-24 20:31:42 +0100

    New module functional Objects. Rational class created. learning-scala pom updated with reference to new module.

---
+++ functional-objects/src/main/scala/org/adzarei/learningscala/functionalobjects/Rational.scala
@@ -0,0 +1,10 @@
+package org.adzarei.learningscala.functionalobjects
+
+// Class that models the behaviour of rational numbers.
+// Class Parameters:
+//   - n represents the 'numerator'
+//   - d represents the 'denominator'
+class Rational(n: Int, d: Int) {
+  // Scala compiler will add any code inside these curly brackets to the constructor.
+  println("Created " + n + "/" + d)
+}
