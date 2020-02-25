d9c0a4a00384873e78bb2bae3847c11704ede1e9
Adrian <adrianzapater@gmail.com>
2020-02-25 21:14:09 +0100

    Rational.scala continued: -> Automatic normalisation of numerator and denominator added. -> New methods: ----> Public: add, +, lessThan, <, greaterThat, >, min and max. ----> Private: grtCommonDivisor

--- functional-objects/src/main/scala/org/adzarei/learningscala/functionalobjects/Rational.scala
+++ functional-objects/src/main/scala/org/adzarei/learningscala/functionalobjects/Rational.scala
@@ -1,20 +1,38 @@
 package org.adzarei.learningscala.functionalobjects
 
+import scala.annotation.tailrec
+
 // Class that models the behaviour of rational numbers.
 // Class Parameters:
 //   - n represents the 'numerator'
 //   - d represents the 'denominator'
+// NOTE: Class Parameters can't be accessed outside the object.
 class Rational(n: Int, d: Int) {
   // Scala compiler will add any code inside these curly brackets to the constructor.
   require(d != 0, "Numerator can't be 0.")
 
-  val num: Int = n
-  val denom: Int = d
+  // This are fields. If public they can be accessed outside the object.
+  private val g = grtCommonDivisor(n, d)
+  val numer: Int = n / g
+  val denom: Int = d / g
 
   override def toString: String = n + "/" + d
 
-  def add (that: Rational): Rational = {
-    new Rational((this.n * that.denom) + (that.num * this.d), this.d * that.denom)
+  def add (that: Rational): Rational = new Rational(
+      this.numer * that.denom + that.numer * this.denom,
+      this.denom * that.denom
+    )
+  def lessThan(that: Rational): Boolean = this.numer * that.denom < that.numer * this.denom
+  def greaterThan (that: Rational): Boolean = this.numer * that.denom > that.numer * this.denom
+  def max(that: Rational): Rational = if( this > that ) this else that
+  def min(that: Rational): Rational = if( this < that ) this else that
+  def + (that: Rational): Rational = add(that)
+  def < (that: Rational): Boolean = lessThan(that)
+  def > (that: Rational): Boolean = greaterThan(that)
+
+  @tailrec
+  private def grtCommonDivisor(a: Int, b: Int): Int = {
+    if (b == 0) a else grtCommonDivisor(b, a % b)
   }
 
 }
